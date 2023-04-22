048a1ade01a2a4caa729d4124c780d06767bfbf6
JevanTeo <116253026+JevanTeo@users.noreply.github.com>
2023-04-22 16:16:50 +0800

    Update Question1.java

--- Question1.java
+++ Question1.java
@@ -2,10 +2,6 @@ public class Question1
 {
   public static void main(String[] args)
   {
-    /**
-     * Print "Hello World!"
-     * System.out.println("Hello World!")
-     * Hint: System.out.println(...)
-     */
+    System.out.println("Hello World!")
   }
 }
