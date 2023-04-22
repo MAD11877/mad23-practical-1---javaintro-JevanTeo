6ab89ee905650636d3f48643d1073bbebe2ea732
JevanTeo <116253026+JevanTeo@users.noreply.github.com>
2023-04-22 21:26:48 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -42,7 +42,7 @@ public class Question5
       int maxCount = 0, maxValue = 0;
       for(int i = 0; i<array.length; i++)
       {
-        int count = 0;
+        int count = 1;
         for(int j = 0; j<array.length; j++)
         {
           if(array[j] == array[i]) count++;
