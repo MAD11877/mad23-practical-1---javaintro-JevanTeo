b6cbc1db8c25bba59a8c3d5ccf0301aea341a028
JevanTeo <116253026+JevanTeo@users.noreply.github.com>
2023-04-23 15:37:06 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -39,13 +39,14 @@ public class Question5
         number_set.add(set);
         }
       array = number_set.toArray(array);
-      int maxCount = 0, maxValue = 0;
+      int maxCount = 0;
+      int maxValue = 0;
       for(int i = 0; i<array.length; i++)
       {
         int count = 0;
         for(int j = 0; j<array.length; j++)
         {
-          if(array[j] == array[i]) count++;
+          if(array[j] == array[i]) {count++;}
         }
         if(count>maxCount){
           maxValue = array[i];
